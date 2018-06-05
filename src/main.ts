28e00a6055aee69bdc4c313d2e3df948f2220d18
azertypow <azertypow@gmail.com>
2018-06-05 15:06:39 +0200

    import new containers/ folder modules in main application files ./main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
+import * as containers from "./containers/_module_export"
 import * as document from "./document/_module_export"
 import * as elements from "./elements/_module_export"
 import * as geometry from "./geometry/_module_export"
 import * as guide    from "./guide/_module_export"
 import * as page     from "./page/_module_export"
 
-export {document, elements, geometry, guide, page}
+export {containers, document, elements, geometry, guide, page}
 
 export {env} from "./env"
 
